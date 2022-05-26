593c6cb4e0f0c58ce3432a7027d3b363e48998ce
Saul <saul_p4rdo@icloud.com>
2022-05-26 04:00:50 -0500

    Definiendo Rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Iniciarsesion from "./layout/Iniciarsesion";
+import Layout from "./layout/Layout";
+import Inicio from "./pages/Inicio";
+import LoginForm from "./pages/LoginForm";
+
 const App = () => {
-  return <h1>CRM REACT</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Iniciarsesion />}>
+          <Route index element={<LoginForm />} />
+        </Route>
+        <Route path="/clientes" element={<Layout />}>
+          <Route index element={<Inicio />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
