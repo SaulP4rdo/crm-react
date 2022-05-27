fb9e17cf59b2ecd4a5eaf96e09d6579494ed1ad5
Saul <saul_p4rdo@icloud.com>
2022-05-26 20:20:05 -0500

    Agrupando Rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Iniciarsesion from "./layout/Iniciarsesion";
 import Layout from "./layout/Layout";
 import Inicio from "./pages/Inicio";
-import LoginForm from "./pages/LoginForm";
-
+import NuevoCliente from "./pages/NuevoCliente";
+import EditarCliente from "./pages/EditarCliente";
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Iniciarsesion />}>
-          <Route index element={<LoginForm />} />
-        </Route>
         <Route path="/clientes" element={<Layout />}>
           <Route index element={<Inicio />} />
+          <Route path="nuevo" element={<NuevoCliente />} />
+          <Route path="editar/:id" element={<EditarCliente />} />
         </Route>
       </Routes>
     </BrowserRouter>
