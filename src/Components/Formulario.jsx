70f55523bfd9fab0383eeeca4c021c4609f1371e
Saul <saul_p4rdo@icloud.com>
2022-06-01 04:03:55 -0500

    Actualizando Registros de la API

--- src/Components/Formulario.jsx
+++ src/Components/Formulario.jsx
@@ -3,8 +3,9 @@ import { Formik, Form, Field } from "formik";
 import { useNavigate } from "react-router-dom";
 import * as Yup from "yup";
 import Alerta from "./Alerta";
+import Spinner from "./Spinner";
 
-const Formulario = () => {
+const Formulario = ({ cliente, cargando }) => {
   const navigate = useNavigate();
   const nuevoClienteSchema = Yup.object().shape({
     nombre: Yup.string()
@@ -22,36 +23,50 @@ const Formulario = () => {
   });
   const handleSubmit = async (valores) => {
     try {
-      const url = "http://localhost:4000/clientes";
-      const respuesta = await fetch(url, {
-        method: "POST",
-        body: JSON.stringify(valores),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      console.log(respuesta);
-      const resultado = await respuesta.json();
-      console.log(resultado);
+      let respuesta;
+      if (cliente.nombre) {
+        // Editando registro
+        const url = `http://localhost:4000/clientes/${cliente.id}`;
+        respuesta = await fetch(url, {
+          method: "PUT",
+          body: JSON.stringify(valores),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+      } else {
+        // Nuevo Registrp
+        const url = "http://localhost:4000/clientes";
+        respuesta = await fetch(url, {
+          method: "POST",
+          body: JSON.stringify(valores),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+      }
+
+      await respuesta.json();
       navigate("/clientes");
-    } catch (error) {
-      console.log("erropr");
-    }
+    } catch (error) {}
   };
-  return (
+  return cargando ? (
+    <Spinner />
+  ) : (
     <div className="bg-white mt-10 px-5 py-10 rounded-md shadow-md md:w-3/4 mx-auto">
       <h1 className="text-gray-600 font-bold text-xl uppercase text-center">
-        Agregar Cliente
+        {cliente?.nombre ? "Editar Cliente" : "Agregar Cliente"}{" "}
       </h1>
 
       <Formik
         initialValues={{
-          nombre: "",
-          empresa: "",
-          email: "",
-          telefono: "",
-          notas: "",
+          nombre: cliente?.nombre ?? "",
+          empresa: cliente?.empresa ?? "",
+          email: cliente?.email ?? "",
+          telefono: cliente?.telefono ?? "",
+          notas: cliente?.notas ?? "",
         }}
+        enableReinitialize={true}
         onSubmit={async (values, { resetForm }) => {
           await handleSubmit(values);
 
@@ -137,7 +152,7 @@ const Formulario = () => {
               </div>
               <input
                 type="submit"
-                value={"Agregar Cliente"}
+                value={cliente?.nombre ? "Editar" : "Agregar Cliente"}
                 className="mt-5 w-full bg-blue-800 p-3 text-white uppercase font-bold text-lg"
               />
             </Form>
@@ -146,6 +161,11 @@ const Formulario = () => {
       </Formik>
     </div>
   );
+};
+
+Formulario.defaulProps = {
+  cliente: {},
+  cargando: false,
 };
 
 export default Formulario;
--- src/pages/EditarCliente.jsx
+++ src/pages/EditarCliente.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import Formulario from "../Components/Formulario";
 
 const EditarCliente = () => {
-  return <div>EditarCliente</div>;
+  const [cliente, setCliente] = useState({});
+  const [cargando, setCargando] = useState(true);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const obtenerClienteAPI = async () => {
+      try {
+        const url = `http://localhost:4000/clientes/${id}`;
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+        setCliente(resultado);
+      } catch (error) {
+        console.log("error");
+      }
+      setCargando(!cargando);
+    };
+    obtenerClienteAPI();
+  }, []);
+
+  return cliente?.nombre ? (
+    <>
+      <h1 className="font-black text-4xl text-blue-900 ">Editar Cliente</h1>
+      <p className="mt-3">Editando datos del cliente</p>
+      <Formulario cliente={cliente} cargando={cargando} />
+    </>
+  ) : (
+    <p className="uppercase">Cliente No Encontrado</p>
+  );
 };
 
 export default EditarCliente;
